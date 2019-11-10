c0297832214e9a95c2fd5ae1856a1052103ac526
Jaakko Korhonen <jaakko.korh@gmail.com>
2019-11-10 08:43:14 +0000

    Mount /boot after launch.

--- board/raspberrypi/post-build.sh
+++ board/raspberrypi/post-build.sh
@@ -9,3 +9,13 @@ if [ -e ${TARGET_DIR}/etc/inittab ]; then
 	sed -i '/GENERIC_SERIAL/a\
 tty1::respawn:/sbin/getty -L  tty1 0 vt100 # HDMI console' ${TARGET_DIR}/etc/inittab
 fi
+
+# Mount /boot/ from sdcard
+if [ ! -d ${TARGET_DIR}/boot ]; then
+    mkdir ${TARGET_DIR}/boot
+fi
+if ! grep -qE '^/dev/mmcblk0p1' ${TARGET_DIR}/etc/fstab; then
+    #     # <file system> <mount pt>      <type>  <options>       <dump>  <pass>
+    echo "/dev/mmcblk0p1  /boot           vfat    defaults        0       2" \
+    >> ${TARGET_DIR}/etc/fstab
+fi
